d31e750c8f87c502f0395b092b6387930034fd7e
Justin Moon <mail@justinmoon.com>
2020-03-23 18:34:05 -0500

    Attempt to run example

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+//fn main() {
+    //println!("Hello, world!");
+//}
+
+use libtor::{Tor, TorFlag, TorAddress, HiddenServiceVersion};
+
+fn main() {
+
+Tor::new()
+    .flag(TorFlag::DataDirectory("/tmp/tor-rust".into()))
+    .flag(TorFlag::SocksPort(19050))
+    .flag(TorFlag::HiddenServiceDir("/tmp/tor-rust/hs-dir".into()))
+    .flag(TorFlag::HiddenServiceVersion(HiddenServiceVersion::V3))
+    .flag(TorFlag::HiddenServicePort(TorAddress::Port(8000), None.into()))
+    .start();
+
+}
